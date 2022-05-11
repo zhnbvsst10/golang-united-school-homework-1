17349a014e425566dadb76a6127cb53bb11df030
zhnbvsst10 <63463825+zhnbvsst10@users.noreply.github.com>
2022-05-11 23:41:18 +0600

    update

--- solution.go
+++ solution.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kyokomi/emoji/v2"
 )
 
@@ -8,5 +9,5 @@ func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
 }
 func main() {
-	GetMessage()
+	fmt.Println(GetMessage())
 }
